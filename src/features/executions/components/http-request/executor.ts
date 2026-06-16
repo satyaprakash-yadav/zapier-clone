528000fad9f7d8f0d8186800957b33e5580d0440
“satyaprakash-yadav” <satyay9798@gmail.com>
2026-06-16 22:41:52 +0530

    22: google form trigger

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -13,9 +13,9 @@ Handlebars.registerHelper("json", (context) => {
 });
 
 type HttpRequestData = {
-    variableName: string;
-    endpoint: string;
-    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName?: string;
+    endpoint?: string;
+    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
 };
 
@@ -33,38 +33,40 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
         }),
     );
 
-    if (!data.endpoint) {
-        await publish(
-            httpRequestChannel().status({
-                nodeId,
-                status: "error",
-            }),
-        );
-        throw new NonRetriableError("HTTP Request node: No endpoint configured");
-    };
 
-    if (!data.variableName) {
-        await publish(
-            httpRequestChannel().status({
-                nodeId,
-                status: "error",
-            }),
-        );
-        throw new NonRetriableError("HTTP Request node: Variable name not configured");
-    };
-
-    if (!data.method) {
-        await publish(
-            httpRequestChannel().status({
-                nodeId,
-                status: "error",
-            }),
-        );
-        throw new NonRetriableError("HTTP Request node: Method name not configured");
-    };
 
     try {
         const result = await step.run("http-request", async () => {
+            if (!data.endpoint) {
+                await publish(
+                    httpRequestChannel().status({
+                        nodeId,
+                        status: "error",
+                    }),
+                );
+                throw new NonRetriableError("HTTP Request node: No endpoint configured");
+            };
+
+            if (!data.variableName) {
+                await publish(
+                    httpRequestChannel().status({
+                        nodeId,
+                        status: "error",
+                    }),
+                );
+                throw new NonRetriableError("HTTP Request node: Variable name not configured");
+            };
+
+            if (!data.method) {
+                await publish(
+                    httpRequestChannel().status({
+                        nodeId,
+                        status: "error",
+                    }),
+                );
+                throw new NonRetriableError("HTTP Request node: Method name not configured");
+            };
+
             // https://.../{{todo.httpResponse.data.userId}}
             const endpoint = Handlebars.compile(data.endpoint)(context);
             const method = data.method || "GET";
--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -6,6 +6,7 @@ import { PAGINATION } from "@/config/constants";
 import { NodeType } from "@/generated/prisma/client";
 import type { Node, Edge } from "@xyflow/react";
 import { inngest } from "@/inngest/client";
+import { sendWorkflowExecution } from "@/inngest/utils";
 
 export const workflowsRouter = createTRPCRouter({
     execute: protectedProcedure
@@ -18,9 +19,8 @@ export const workflowsRouter = createTRPCRouter({
                 },
             });
 
-            await inngest.send({
-                name: "workflows/execute.workflow",
-                data: { workflowId: input.id },
+            await sendWorkflowExecution({
+                workflowId: input.id,
             });
 
             return workflow;
